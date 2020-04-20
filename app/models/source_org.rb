f0f5c5615ff4591b5283e3aed8881cd194da4dc4
James Trinder <jamesanthonyt@live.co.uk>
2020-04-20 12:27:09 +0100

    SourceOrg import uses same logic as Af Venues
    Previously all SourceOrgs were deleted and reimported but this fails due to the
    foreign key constraint. Now the importer checks if the SourceOrg already exists,
    if not then it will create a record.

--- app/models/source_org.rb
+++ app/models/source_org.rb
@@ -4,11 +4,21 @@ class SourceOrg < ApplicationRecord
   has_many :af_venue_mappings
   validates :name, presence: true, uniqueness: true
 
+  require 'csv'
+
   def self.import_af_source_orgs
-    require 'csv'
-    SourceOrg.destroy_all
-    CSV.foreach(Rails.root.join('lib/af-source-orgs.csv'), headers: true) do |row|
-      SourceOrg.create(id: row[0], name: row[1])
+    csv = CSV.read('lib/af-source-orgs.csv', headers: true, encoding:'iso-8859-1:utf-8')
+    csv.each do |row|
+      next if source_org_found?(row)
+
+      SourceOrg.create(
+        id: row[0],
+        name: row[1]
+      )
     end
+  end
+
+  def self.source_org_found?(row)
+    SourceOrg.find_by(id: row[0])
   end
 end
