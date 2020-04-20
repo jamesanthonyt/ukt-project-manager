963ffd606c7f159c77594cad024e9f4b9eac7b0a
James Trinder <jamesanthonyt@live.co.uk>
2020-04-20 12:49:48 +0100

    SourceOrg import now updated the avaiability of a SourceOrgId
    This now matches AfVenues import so that if a SourceOrgId is not included in the
    CSV it will be updated as deleted: true. If in a subsequent upload the Id is found
    again then the SourceOrg will be updated as deleted: false

--- app/models/source_org.rb
+++ app/models/source_org.rb
@@ -9,16 +9,37 @@ class SourceOrg < ApplicationRecord
   def self.import_af_source_orgs
     csv = CSV.read('lib/af-source-orgs.csv', headers: true, encoding:'iso-8859-1:utf-8')
     csv.each do |row|
-      next if source_org_found?(row)
+      next if source_org_found_available?(row)
+
+      source_org_available!(row) if source_org_found_unavailable?(row)
 
       SourceOrg.create(
         id: row[0],
         name: row[1]
       )
     end
+    source_org_unavailable!(csv)
   end
 
-  def self.source_org_found?(row)
-    SourceOrg.find_by(id: row[0])
+  def self.source_org_unavailable!(csv)
+    source_orgs = SourceOrg.all
+    source_orgs.each do |org|
+      if csv.find { |row| (row[0].to_s == org.id.to_s) } == nil
+        org.update!(deleted: true)
+      end
+    end
+  end
+
+  def self.source_org_found_available?(row)
+    SourceOrg.find_by(id: row[0], deleted: false)
+  end
+
+  def self.source_org_found_unavailable?(row)
+    SourceOrg.find_by(id: row[0], deleted: true)
+  end
+
+  def self.source_org_available!(row)
+    source_org = SourceOrg.find_by(id: row[0], deleted: true)
+    source_org.update!(deleted: false)
   end
 end
